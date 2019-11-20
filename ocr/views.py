3cb5179f8ef0b1396a98391629f92de65dc116ba
aviad <aviadm24@gmail.com>
2019-11-20 16:26:36 +0200

    check 2

--- ocr/views.py
+++ ocr/views.py
@@ -90,8 +90,8 @@ def image_upload(request):
 
             fs = FileSystemStorage()
             filename = fs.save('ocr/static/' + myfile.name, myfile)
-            uploaded_file_url = fs.path(filename)
-            print(uploaded_file_url)
+            for filename in os.listdir(image_path):
+                print('file: ', filename)
             uploaded_file_url = fs.url(filename)
             print(uploaded_file_url)
             text = plain_ocr(uploaded_file_url)
