0ae2fec07302d58b01ffb4dd5de7245adcc2e2d7
aviad <aviadm24@gmail.com>
2019-11-21 11:19:57 +0200

    check7

--- ocr/views.py
+++ ocr/views.py
@@ -77,7 +77,7 @@ def image_upload(request):
         print(uploaded_file_url)
         text = plain_ocr(uploaded_file_url)
         cheshbonit = close_match(text)
-        # uploaded_file_url = '/'.join(fs.url(filename).split('/')[2:])
+        uploaded_file_url = '/'.join(fs.url(filename).split('/')[2:])
         print(uploaded_file_url)
         return render(request, 'ocr/image_upload.html', {
             'text': text,
